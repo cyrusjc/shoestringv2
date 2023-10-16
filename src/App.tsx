2b83cee5f173d37ba8c4b0a30e8a531089531168
Cyrus Cheung <cyrus.cheung123@gmail.com>
2023-10-15 18:44:37 -0700

    actual first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import BasicExample from "./components/header";
+
+function App() {
+  // const items = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+  // const handleSelectItem = (item : string) => {
+  //   console.log(item)
+  // }
+
+  return (
+    <div>
+      <BasicExample />
+    </div>
+  );
+}
+
+export default App;
