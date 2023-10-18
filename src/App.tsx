472e55c8c7113b64af0bb1e69f76992b66f09fe1
Cyrus Cheung <cyrus.cheung123@gmail.com>
2023-10-17 22:38:47 -0700

    nav bar template done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <NavBar />
+
         <Routes>
           <Route index element={<Home />} />
         </Routes>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,39 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./NavBar.css";
+
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <Navbar className="bg-body-tertiary">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/public/vite.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          React Bootstrap
+        </Navbar.Brand>
+        <Container>
+          <Nav.Link className="navbaritem" href="#home">
+            Link 1{" "}
+          </Nav.Link>
+          <Nav.Link className="navbaritem" href="#home">
+            Link 2{" "}
+          </Nav.Link>
+          <Nav.Link className="navbaritem" href="#home">
+            Link 3{" "}
+          </Nav.Link>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text className="navbaritem">Phone #</Navbar.Text>
+            <Navbar.Text className="navbaritem">IG N SHIT</Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default NavBar;
