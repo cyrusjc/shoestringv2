e67f23aa621652feb7544202b4c0ff943ac68a75
Cyrus Cheung <cyrus.cheung123@gmail.com>
2023-10-19 23:53:41 -0700

    buttons for home and placeholder bg image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import background from "/pic.jpg";
+import Button from "react-bootstrap/Button";
+
+import "./Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="background">
+        <Container className="homebox">
+          <div className="centerbuttons">
+            <Row>
+              <Col>
+                <Button className="homebutton" variant="light">
+                  Link 1
+                </Button>
+              </Col>
+              <Col>
+                <Button className="homebutton" variant="light">
+                  Link 2
+                </Button>
+              </Col>
+              <Col>
+                <Button className="homebutton" variant="light">
+                  Link 3
+                </Button>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
 }
 
 export default Home;
