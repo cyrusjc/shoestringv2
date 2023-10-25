65c45bc9686cea4153621af9715a6a7ee0338c75
Cyrus Cheung <cyrus.cheung123@gmail.com>
2023-10-25 15:58:31 -0700

    fixed some warnings

--- src/components/Figure/Figure.tsx
+++ src/components/Figure/Figure.tsx
@@ -1,14 +1,10 @@
+import path from "path";
 import Figure from "react-bootstrap/Figure";
 
 function FigureExample(desc: string, pathToPic: string) {
   return (
     <Figure>
-      <Figure.Image
-        width={171}
-        height={180}
-        alt="171x180"
-        src="holder.js/171x180"
-      />
+      <Figure.Image width={171} height={180} alt="171x180" src={pathToPic} />
       <Figure.Caption>{desc}</Figure.Caption>
     </Figure>
   );
