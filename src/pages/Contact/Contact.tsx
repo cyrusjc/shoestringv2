99231ae4e8624783ff6024f55954871258eadeda
Cyrus Cheung <cyrus.cheung123@gmail.com>
2023-10-26 21:28:56 -0700

    embedded goggles pic

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Container } from "react-bootstrap";
 
 const Contact = () => {
-  return <div className="background">Contact</div>;
+  return (
+    <div className="background">
+      <Container>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d461.0524810393475!2d-123.06972116073673!3d49.17956213042419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5486759e6c21d885%3A0x5e2ef173da2a3c86!2sThe%20Shoestring%20Cafe!5e0!3m2!1sen!2sca!4v1698292193979!5m2!1sen!2sca"
+          width="600"
+          height="450"
+          loading="lazy"
+        />
+      </Container>
+      Contact
+    </div>
+  );
 };
 
 export default Contact;
