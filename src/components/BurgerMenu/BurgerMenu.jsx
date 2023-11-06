df6004fef4691c1bded55b28719f147f7d4ade1c
Tatiana Privezentseva <severny.island@gmail.com>
2023-11-05 16:40:29 -0800

    finish the navbar for mobile and the modal for the menu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './BurgerMenu.scss'; 
+import React, { useState } from "react";
+import "./BurgerMenu.scss";
 
 const BurgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,21 +8,30 @@ const BurgerMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className={`burger-menu ${isOpen ? 'open' : ''}`}>
+    <div className={`burger-menu ${isOpen ? "open" : ""}`}>
       <div className="burger-menu-button" onClick={toggleMenu}>
         <span className="burger-menu-button__bar"></span>
         <span className="burger-menu-button__bar"></span>
         <span className="burger-menu-button__bar"></span>
       </div>
       {isOpen && (
-        <div className="menu-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Menu</li>
-            <li>Hours & Location</li>
-          </ul>
+        <div className="menu-modal">
+          <div className="menu-items">
+            <span className="exit-button" onClick={closeMenu}>
+              &times;
+            </span>
+            <ul className="menu-items__list">
+              <li className="menu-items__items">Home</li>
+              <li className="menu-items__items">About</li>
+              <li className="menu-items__items">Menu</li>
+              <li className="menu-items__items">Hours & Location</li>
+            </ul>
+          </div>
         </div>
       )}
     </div>
