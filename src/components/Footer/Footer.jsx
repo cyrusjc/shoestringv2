eeb98c02ec289e0c3a41bc5d03c726e4b7f5a2a5
Tatiana Privezentseva <severny.island@gmail.com>
2023-12-09 20:51:47 -0800

    add another section with info to the main page, styling could be temp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,46 +8,32 @@ import { FaFacebook, FaYelp } from "react-icons/fa";
 export default function FooterWithLogo() {
   return (
     <>
-    <div className="information">
-    <h2>Hours and Location</h2>
-      <h3>Dinner</h3>
-      <p>10pm</p>
-      <h3>Lunch</h3>
-      <p>Currently Closed</p>
-
-      <h2>Contact </h2>
-      <p> 604 232-0900</p>
-      <p>shoestringcafe@gmail.com</p>
-      
-      <h2>Reservations</h2>
-      <p> Please call our phone number to make reservations.</p>
-    </div>
-    <footer className="text-center text-lg-start footer main-colour-text">
-      <Container className="text-center">
-        <Row>
-          <div className="title-row">
-            <img
-              alt=""
-              src="/public/vite.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            Shoestring
-          </div>
-        </Row>
-        <Row>
-          <div className="icons-row">
-            <Icons url="//www.google.com" icon={<FiInstagram />} />
-            <Icons url="//www.google.com" icon={<FaFacebook />} />
-            <Icons url="//www.google.com" icon={<FaYelp />} />
-          </div>
-        </Row>
-        <Row className="trademark-row">
-          <div>© {new Date().getFullYear()} Shoestring Cafe</div>
-        </Row>
-      </Container>
-    </footer>
+      <footer className="text-center text-lg-start footer main-colour-text">
+        <Container className="text-center">
+          <Row>
+            <div className="title-row">
+              <img
+                alt=""
+                src="/public/vite.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              Shoestring
+            </div>
+          </Row>
+          <Row>
+            <div className="icons-row">
+              <Icons url="//www.google.com" icon={<FiInstagram />} />
+              <Icons url="//www.google.com" icon={<FaFacebook />} />
+              <Icons url="//www.google.com" icon={<FaYelp />} />
+            </div>
+          </Row>
+          <Row className="trademark-row">
+            <div>© {new Date().getFullYear()} Shoestring Cafe</div>
+          </Row>
+        </Container>
+      </footer>
     </>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,9 +17,33 @@ function Home() {
           </p>
           <Link to="/Menu">
             <button className="container__button">View our menu</button>
-            </Link>
+          </Link>
         </div>
-      {/* <div className="information">
+        <div className="section">
+          <div className="section__container">
+            <h2 className="section__heading">Hours & Location</h2>
+            <div className="section__times">
+              <h3 className="section__subheading">Dinner:</h3>
+              <p className="section__paragraph section__hours">10pm</p>
+              <h3 className="section__subheading section__lunch">Lunch:</h3>
+              <p className="section__paragraph">Currently Closed</p>
+            </div>
+          </div>
+          <div className="section__container">
+            <h2 className="section__heading">Contact </h2>
+            <div className="section__info">
+              <p className="section__paragraph">604-232-0900</p>
+              <p className="section__paragraph">shoestringcafe@gmail.com</p>
+            </div>
+          </div>
+          <div className="section__container">
+            <h2 className="section__heading">Reservations</h2>
+            <p className="section__paragraph">
+              Please call our phone number to make reservations.
+            </p>
+          </div>
+        </div>
+        {/* <div className="information">
           <h2>Hours and Location</h2>
             <h3>Dinner</h3>
             <p>10pm</p>
@@ -33,8 +57,7 @@ function Home() {
             <h2>Reservations</h2>
             <p> Please call our phone number to make reservations.</p>
           </div> */}
-        </div>
-     
+      </div>
     </>
   );
 }
