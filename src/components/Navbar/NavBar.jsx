5d040fd42a65d91b9db1cdfa97800b71172be545
Tatiana Privezentseva <severny.island@gmail.com>
2024-01-02 17:35:49 -0800

    fix the navbar jerk

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,20 +1,15 @@
-import "./NavBar.scss";
 
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
+import "./NavBar.scss";
 
 function NavBar() {
   const [navbar, setNavbar] = useState(false);
 
   useEffect(() => {
     const changeBackground = () => {
-      if (window.scrollY > 70) {
-        setNavbar(true);
-      } else {
-        setNavbar(false);
-      }
+      setNavbar(window.scrollY > 70);
     };
 
     window.addEventListener("scroll", changeBackground);
@@ -26,40 +21,29 @@ function NavBar() {
   }, []);
 
   return (
-    <>
-      <header
-        className={
-          navbar ? "primary-header primary-header__bg" : "primary-header"
-        }
-      >
-        <div className="primary-header__inner">
-          <span className="logo">
-            <Link to="/" title="home">
-              {/* <img
-            src={logo}
-            className="navbar__logo"
-            alt="logo"
-          /> */}
-              <h1 className="logo">Shoestring Café</h1>
-            </Link>
-          </span>
-          <div className="primary-header__nav-links">
-            <Link to="/" title="home" className="primary-header__buttons">
-              Home
-            </Link>
-            <Link to="/Menu" title="menu" className="primary-header__buttons">
-              Menu
-            </Link>
-            <Link to="/" title="home" className="primary-header__buttons">
-              About
-            </Link>
-          </div>
-          <div className="primary-header__hamburg">
-            <BurgerMenu />
-          </div>
+    <header className={navbar ? "primary-header primary-header__bg" : "primary-header"}>
+      <div className="primary-header__inner">
+        <span className="logo">
+          <Link to="/" title="home">
+            <h1 className="logo">Shoestring Café</h1>
+          </Link>
+        </span>
+        <div className="primary-header__nav-links">
+          <Link to="/" title="home" className="primary-header__buttons">
+            Home
+          </Link>
+          <Link to="/Menu" title="menu" className="primary-header__buttons">
+            Menu
+          </Link>
+          <Link to="/" title="home" className="primary-header__buttons">
+            About
+          </Link>
         </div>
-      </header>
-    </>
+        <div className="primary-header__hamburg">
+          <BurgerMenu />
+        </div>
+      </div>
+    </header>
   );
 }
 
