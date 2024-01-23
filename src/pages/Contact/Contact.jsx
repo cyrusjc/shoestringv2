452f5786f413f28cc03df862c39701288c194bf8
Cyrus Cheung <cyrus.cheung123@gmail.com>
2024-01-23 03:35:52 -0800

    styling change

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -56,7 +56,7 @@ const Contact = () => {
   };
 
   return (
-    <div className="background">
+    <div className="container_wrapper">
       <div className="container_contact">
         <div className="container_contact_element">
           <div className="container_contact_forms">
@@ -76,84 +76,78 @@ const Contact = () => {
                 onSubmit={handleSubmit}
                 className="container_contact_forms_field"
               >
-                <div className="mb-3 container_contact_forms_split">
-                  <Form.Group
-                    as={Col}
-                    md="5"
-                    controlId="validationCustom01"
-                    className="mb-3 container_contact_forms_split_item"
-                  >
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                      required
-                      type="text"
-                      placeholder="Name"
-                      onChange={(e) =>
-                        setFormData({ ...formData, name: e.target.value })
-                      }
-                    />
-                    <Form.Control.Feedback></Form.Control.Feedback>
-                  </Form.Group>
-                  <Form.Group
-                    as={Col}
-                    md="5"
-                    controlId="validationCustom02"
-                    className="mb-3 container_contact_forms_split_item"
-                  >
-                    <Form.Label>Phone (optional)</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder=""
-                      onChange={(e) =>
-                        setFormData({ ...formData, phone: e.target.value })
-                      }
-                    />
-                  </Form.Group>
-                </div>
-                <Row className="mb-3">
-                  <Form.Group
-                    as={Col}
-                    md="10"
-                    controlId="validationCustom03"
-                    className="container_contact_forms_field"
-                  >
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                      type="email"
-                      placeholder="email@email.com"
-                      onChange={(e) =>
-                        setFormData({ ...formData, email: e.target.value })
-                      }
-                      required
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      Please provide your contact email so we can reach you.
-                    </Form.Control.Feedback>
-                  </Form.Group>
-                </Row>
-                <Row className="mb-3">
-                  <Form.Group
-                    as={Col}
-                    md="10"
-                    controlId="validationCustom04"
-                    className="container_contact_forms_field"
-                  >
-                    <Form.Label>What is your question?(*)</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      type="text"
-                      rows={3}
-                      placeholder="Question here!"
-                      onChange={(e) =>
-                        setFormData({ ...formData, message: e.target.value })
-                      }
-                      required
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      Please fill this field.
-                    </Form.Control.Feedback>
-                  </Form.Group>
-                </Row>
+                {/* <div className="container_contact_forms_split"> */}
+                <Form.Group
+                  as={Col}
+                  controlId="validationCustom01"
+                  className="container_contact_forms_field"
+                >
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Name"
+                    onChange={(e) =>
+                      setFormData({ ...formData, name: e.target.value })
+                    }
+                  />
+                  <Form.Control.Feedback></Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group
+                  as={Col}
+                  controlId="validationCustom02"
+                  className="container_contact_forms_field"
+                >
+                  <Form.Label>Phone (optional)</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder=""
+                    onChange={(e) =>
+                      setFormData({ ...formData, phone: e.target.value })
+                    }
+                  />
+                </Form.Group>
+                {/* </div> */}
+
+                <Form.Group
+                  as={Col}
+                  controlId="validationCustom03"
+                  className="container_contact_forms_field"
+                >
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="email@email.com"
+                    onChange={(e) =>
+                      setFormData({ ...formData, email: e.target.value })
+                    }
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide your contact email so we can reach you.
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group
+                  as={Col}
+                  controlId="validationCustom04"
+                  className="container_contact_forms_field"
+                >
+                  <Form.Label>What is your question?(*)</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    type="text"
+                    rows={3}
+                    placeholder="Question here!"
+                    onChange={(e) =>
+                      setFormData({ ...formData, message: e.target.value })
+                    }
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please fill this field.
+                  </Form.Control.Feedback>
+                </Form.Group>
 
                 <Button
                   type="button"
@@ -166,14 +160,14 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <div className="container_contact_element">
+        {/* <div className="container_contact_element">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d461.0524810393475!2d-123.06972116073673!3d49.17956213042419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5486759e6c21d885%3A0x5e2ef173da2a3c86!2sThe%20Shoestring%20Cafe!5e0!3m2!1sen!2sca!4v1698292193979!5m2!1sen!2sca"
             width="600"
             height="600"
             loading="lazy"
           />
-        </div>
+        </div> */}
 
         <Modal
           show={show}
